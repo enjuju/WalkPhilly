97ba9f3a43d5ccff9f73e9f4a9a4020f4d65b88b
enjuju <pkamlley@gmail.com>
2023-06-08 14:51:36 -0700

    Changed img src references to https://

--- walk-city/vue/src/components/HistoryDisplay.vue
+++ walk-city/vue/src/components/HistoryDisplay.vue
@@ -26,7 +26,7 @@
         <h2 id="location-name">{{ locationObject.locationName }}</h2>
         <div class="cool-line"></div>
         <img id="location-image"
-          :src="`http://walk-philly.fly.dev/api/photos/Philadelphia ${locationObject.locationName}`" alt="" />
+          :src="`https://walk-philly.fly.dev/api/photos/Philadelphia ${locationObject.locationName}`" alt="" />
         <div id="location-description">
           <p id="description">{{ locationObject.description }}</p>
           <p id="availability">{{ locationObject.availability }}</p>
